81d75f01d544be6194975390d949a1c4e17cbc5e
emp9wer <119460075+emp9wer@users.noreply.github.com>
2023-05-03 19:53:21 -0400

    added comment

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) { // loop
             System.out.println("hello");
         }
 
